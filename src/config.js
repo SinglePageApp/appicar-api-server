b1ba0746b8e64f8f837068305ce1ccc4e75932bd
Matías J. Magni <matias.magni@gmail.com>
2018-02-27 20:27:44 -0300

    API-4: CORS options segregated in dev and prod env. Schemas added/modif.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,18 @@
 export default {
-  PORT: 9000,
-  MONGO_URL: 'mongodb://mongodb.localhost:27017/appicar'
+  'dev': {
+    PORT: 9000,
+    MONGO_URL: 'mongodb://localhost:27017/appicar',
+    CORS_OPTIONS: {
+      origin: '*',
+      credentials: true
+    }
+  },
+  'prod': {
+    PORT: 9000,
+    MONGO_URL: 'mongodb://mongodb.localhost:27017/appicar',
+    CORS_OPTIONS: {
+      origin: 'http://appicar.com',
+      credentials: true
+    }
+  }
 }
