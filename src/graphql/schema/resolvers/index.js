9dcf6f4f02269abb2bb89ad5b300e9aecfcb5f68
Matías J. Magni <matias.magni@gmail.com>
2018-03-18 22:47:43 -0300

    API-16: Find stores using pagination done.

--- src/graphql/schema/resolvers/index.js
+++ src/graphql/schema/resolvers/index.js
@@ -29,25 +29,38 @@ const resolvers = {
 
       return store
     },
+    storesCount: async (parent, args, { Store }) => {
+      return Store.count()
+    },
     stores: async (parent, args, { Store }) => {
       let stores = []
+      let limit = 0
+      let skip = 0
+
+      if (Number.isSafeInteger(args.limit) && Number.isSafeInteger(args.skip)) {
+        limit = args.limit
+        skip = args.skip
+        delete args.limit
+        delete args.skip
+      }
+
       if (args.menuItemType) {
         stores = await Store.find({
           ['menu.items.' + args.menuItemType.toLowerCase() + '.category']: {
             $regex: '.*' + args.menuItemCategory + '.*',
             $options: 'i'
           }
-        })
+        }).skip(skip).limit(limit)
       } else {
-        stores = await Store.find(args)
+        stores = await Store.find(args).skip(skip).limit(limit)
       }
 
       return stores
     },
     featuredStores: async (parent, args, { Store }) => {
-      let store = await Store.find({ featured: true })
+      let stores = await Store.find({ featured: true })
 
-      return store
+      return stores
     }
   },
   Mutation: {
@@ -58,8 +71,12 @@ const resolvers = {
       let successful = false
       // send the message and get a callback with an error or details of the message that was sent
       server.send(mailOptions, (err, message) => {
-        console.log(err)
-        if (!err) {
+        console.log('E-mail data: ')
+        console.log(mailOptions)
+        // Error handling.
+        if (err) {
+          console.log(err)
+        } else {
           successful = true
         }
       })
